b46b9408bc90ce86451a4e578bf7cad41c0ba2f0
aojaswi <abhiram.ojaswi@wissen.com>
2025-01-23 12:48:09 +0530

    Simple Java application added

---
+++ TestDemo.java
@@ -0,0 +1,9 @@
+class TestDemo
+{
+    public static void main (String args[])
+    {
+        System.out.println("Jenkins is used for CICD");
+        System.out.println("Welcome to Jenkins");
+    }
+}
+ 
