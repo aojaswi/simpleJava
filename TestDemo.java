565640af3ee2c90bc0708f68584c1af815dcc245
aojaswi <abhiram.ojaswi@wissen.com>
2025-01-23 12:54:35 +0530

    modified

--- TestDemo.java
+++ TestDemo.java
@@ -4,7 +4,7 @@ class TestDemo
     {
         System.out.println("Jenkins is used for CICD");
         System.out.println("Welcome to Jenkins");
-	System.out.println("Triggered github webhook");
+	System.out.println("Triggered github webhook to build using jenkins");
     }
 }
  
